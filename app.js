6daf52518ab855124eea76048329deeb8982f777
ARJIT <arjitbhandari222830@gmail.com>
2020-01-30 18:21:48 +0530

    Adding content

--- app.js
+++ app.js
@@ -2,32 +2,24 @@ var express= require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+const {Register,validate}= require('./register');
+
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 mongoose.connect('mongodb://localhost:27017/glitch', {useNewUrlParser: true});
-
-//Schema Setup.
-var glitchSchema = new mongoose.Schema({
-	name: String, 
-	member1: String, 
-	member2: String
-});
-
-var register = mongoose.model("register", glitchSchema);
-
-
 
 
 //adding css files to the node server.
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+app.use(express.json());
+
 app.use(express.static('public'));
-
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set("view engine", "ejs");
 
-
-
-
-// ROUTING PART STARTS HERE
 app.get("/", function(req, res){
 	res.render("index");
 });
@@ -36,36 +28,33 @@ app.get("/register", function(req, res){
 	res.render("register");
 });
 
-app.post("/register",urlencodedParser, function(req, res){
-	//get data from form and add to glitch array.
-	var name = req.body.name;
-	var membr1 = req.body.mname;
-	var membr2 = req.body.m2name;
-	var data = {name: name, member1: membr1, member2: membr2};
-	//Create a new entry and save it to DB
-	register.create(data, function(err, newlyCreated){
-		if(err){
-			console.log(err);
-		} else{
-			// redirect to campground page
-			res.redirect("/"); 
-		}
-	});		 
+app.post('/register',async (req,res)=>{
+    const {error}= validate(req.body);//result.error(joi package)
+    if(error)
+        return res.status(400).send(error.details[0].message);
+    
+    const register= new Register({
+        name: req.body.name,
+		member1: req.body.member1,
+		member2: req.body.member2,
+    });
+    
+    await register.save();
+    
+    res.redirect("/");
 });
-
 
 app.get("/quiz", function(req, res){
-	res.render("questions");
+	res.render("questions_round1");
 });
 
-
+app.get("/quiz/final", function(req, res){
+	res.render("questions_round2");
+});
 
 app.get("/:randomText", function(req, res){
 	res.render("other");
 });
-
-
-
 
 //LISTNING PORT INFORMATION
  app.listen(3000, "0.0.0.0", function(){
